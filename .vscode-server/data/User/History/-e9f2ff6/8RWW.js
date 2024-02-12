ca1860b34851374370c009ae25d45a43f13d9a43
Zhang Chen <zhchen3@millersville.edu>
2024-02-12 20:58:31 +0000

    cleanup

--- .vscode-server/data/User/History/-e9f2ff6/8RWW.js
+++
@@ -1,7 +0,0 @@
-module.exports.list = function(req, res){
-  res.render('blogList', { title: 'Blog List' });
-};
-
-module.exports.add = function(req, res){
-  res.render('blogAdd', { title: 'Add Blog' });
-};
