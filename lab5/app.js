db3dd6eb1f28a3cd7fb4bee0099c17b1383d85ae
Debian <bitnami@ip-172-26-2-38.us-east-2.compute.internal>
2024-03-19 20:02:34 +0000

    lab5

--- lab5/app.js
+++ lab5/app.js
@@ -1,3 +1,4 @@
+module.exports = app;
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,24 +7,30 @@ var logger = require('morgan');
 require('./app_api/models/db');
 
 
-var routes = require('./app_server/routes/index');
+//var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'./app_server', 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname,'./app_server', 'views'));
+//app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
+
+
+//app.use('/', routes);
 app.use('/api', routesApi);
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});  
 
 
 
